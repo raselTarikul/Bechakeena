09798b2d1f93b46ceae0cbbc150d2543848a20f2
Tarikul Islam Rasel <ti.rasel@gmail.com>
2019-01-11 15:44:45 +0600

    Change static directory settings

--- BechaKeena/settings.py
+++ BechaKeena/settings.py
@@ -123,11 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [STATIC_DIR, ]
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 REST_FRAMEWORK = {
