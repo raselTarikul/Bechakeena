4a778c9647920b1becdeebfdc3eca1314f13aa62
Tarikul Islam Rasel <ti.rasel@gmail.com>
2019-01-13 23:05:16 +0600

    mysql database added

--- BechaKeena/settings.py
+++ BechaKeena/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'BechaKeena.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bechakeena',
+        'USER': 'root',
+        'PASSWORD': 'root2019',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
