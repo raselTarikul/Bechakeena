8656a27cfb8f199b34ff331c4d14ab6972e922aa
Tarikul Islam Rasel <ti.rasel@gmail.com>
2019-01-18 13:06:47 +0600

    Login Url changed

--- BechaKeena/urls.py
+++ BechaKeena/urls.py
@@ -13,20 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-
-from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
-from django.conf import settings
-from django.conf.urls.static import static
+from apps import views
 
 
 schema_view = get_swagger_view(title='Bechakena API v1')
 
 
 urlpatterns = [
+    path('', views.LogIn.as_view()),
     path('rest-auth', include('rest_framework.urls')),
     path('swagger/', schema_view),
     path('admin/', admin.site.urls),
