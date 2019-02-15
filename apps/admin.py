04a3f952e5110dde0b535e7977175697473aab94
Tarikul Islam Rasel <ti.rasel@gmail.com>
2019-02-15 22:39:25 +0600

    Admin added

--- apps/admin.py
+++ apps/admin.py
@@ -3,8 +3,8 @@ from .models import Category, Product, Order, OrderLine, Device
 
 
 # Register your models here.
-# admin.site.register(Category)
-# admin.site.register(Product)
-# admin.site.register(Order)
-# admin.site.register(OrderLine)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderLine)
 admin.site.register(Device)
